a98e19987eda650b046a8d62f006ac52fb5de4dc
cmzluna <claudiomazzoli@gmail.com>
2023-09-23 18:31:16 -0300

    persistor

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,23 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
 import { Text, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { signOut } from "../../store/slices/user";
-import type { RootState } from "../../store";
-export default function Index(): React.JSX.Element {
-  const dispatch = useDispatch();
-  const { user } = useSelector((state: RootState) => state.user);
 
+export default function Index(): React.JSX.Element {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{user?.name}</Text>
-      <Text
-        onPress={() => {
-          // The `app/(app)/_layout.tsx` will redirect to the login screen.
-          dispatch(signOut({}));
-        }}
-      >
-        Sign Out
-      </Text>
+      <Text>Llega aqui</Text>
     </View>
   );
 }
